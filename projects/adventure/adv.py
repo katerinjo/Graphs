e6404ad15b832ed173aa2d1377c5340c5c7f6d6e
Catherine Stewart <catherine.c.stwrt@gmail.com>
2020-01-23 19:27:59 -0600

    success!

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from collections import deque
 
 # Load world
 world = World()
@@ -28,8 +29,60 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+'''
+tree = {}
 
+def add_node(node_id):
+    tree[node_id] = {}
 
+def add_edge(from_n, to_n, direction):
+    tree[from_n][direction] = to_n
+    tree[to_n]['anti' + direction] = from_n # not sure backtracking needed
+'''
+
+def df_recursive(room):
+    visited = set()
+    path = []
+
+    def df_recur(room, from_direction=None):
+        visited.add(room.id)
+        for direction in room.get_exits():
+            next_room = room.get_room_in_direction(direction)
+            if next_room.id in visited:
+                continue
+            visited.add(next_room.id)
+            path.append(direction)
+            df_recur(
+                room.get_room_in_direction(direction),
+                from_direction=direction)
+        if from_direction is not None:
+            other_way = {
+                'n': 's',
+                's': 'n',
+                'e': 'w',
+                'w': 'e',
+            }[from_direction]
+            path.append(other_way)
+
+    df_recur(room)
+    return path
+
+traversal_path = df_recursive(world.starting_room)
+print(traversal_path)
+'''
+queue = deque()
+queue.append(player.current_room)
+visited = set()
+while len(queue) > 0:
+    room = queue.popleft()
+    if room not in visited:
+        add_node(room.id)
+        add_edge(previous_id, room.id, some_direction)
+        visited.add(room.id)
+        for next_vertex in self.get_neighbors(vertex):
+            new_path = path + [next_vertex]
+            queue.enqueue(new_path)
+'''
 
 # TRAVERSAL TEST
 visited_rooms = set()
